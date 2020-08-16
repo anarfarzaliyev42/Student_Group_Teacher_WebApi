37ce9ae3fc32706cbb455e3e8d59b8d3b0407ea0
anarfarzaliyev9 <anarfarzaliyev9@gmail.com>
2020-08-16 14:45:59 +0400

    Add project files.

---
+++ StudentGroupWebApi/Abstractions/IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using StudentGroupWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentGroupWebApi.Abstractions
+{
+    public interface IStudentRepository:IGeneralRepo<Student>
+    {
+        bool AddStudentToGroup(int studentId,int groupId);
+    }
+}
